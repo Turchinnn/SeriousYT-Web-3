0b00181ae0447024eee3aa4bb995b8fb97a8bc28
Turchinnn <sven.doring12310@gmail.com>
2025-10-07 00:42:33 +0200

    final 014
    dist update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,10 +10,24 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "src/assets/*",
+          dest: "assets", // kopira u dist/assets
+        },
+      ],
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    assetsInlineLimit: 0, // sprječava Base64 inline slike
   },
 }));
