aeb057835da506c3b460e422eae28dd9c25a65dd
Turchinnn <sven.doring12310@gmail.com>
2025-10-12 06:18:25 +0200

    remove update pass

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 /**
